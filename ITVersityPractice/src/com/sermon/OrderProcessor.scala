7539890dc33009bef1e7894762df39350b23e2c4
foxsermon <foxsermon@yahoo.com>
2018-02-25 17:52:43 -0800

    Initial Scala

--- ITVersityPractice/src/com/sermon/OrderProcessor.scala
+++ ITVersityPractice/src/com/sermon/OrderProcessor.scala
@@ -9,26 +9,52 @@ object OrderProcessor {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
     
-    val orders = sc.textFile("/Users/serranm1/temp/miGIT/pythonPractice/datos/orders.txt")
-    
+    val orders = sc.textFile("/Users/conejitos/marcos/data/orders.txt")
+    /*  
+    val orderRDD = orders.map{t =>                
+      val p = t.split(",")
+      (p(0).toInt, p(1).toString())
+    }
+    */
+
     // orders.map(x => (x(0), 1)).foreach(x => println(x))
     
     //val orderRDD = orders.map(x => parseNames(x))
-    val orderRDD = orders.map(parseNames)
-    println("Counter => " + orderRDD.count())
+    val orderRDD = orders.map{parseNames}
     
-    //orderStatusTop100(orderRDD)
-    orderRDD.map( (x) => new Tuple2(x, 1)).foreach(println(_))
+    orderStatusTop100(orderRDD)
+    //orderRDD.map( x => (x._2, 1)).reduceByKey( (x, y) => x + y ).sortByKey().foreach(println(_))
     
-    println("Salio")
+    println("-------------------------------------------------------------------")
+    orderDate(orderRDD)
   }
   
-  def parseNames(line : String) {
-    val fields = line.split(',')
-    return new Tuple2(fields(0).toInt, fields(1).toString())
+  def parseNames(line : String) : (Int, (String, Int, String)) = {
+    val fields = line.split(",")
+    (fields(0).toInt, (fields(1).toString(), fields(2).toInt, fields(3).toString()))
   }
   
-  def orderStatusTop100(orderRDD : RDD[Unit]) {
-    val orderStatus = orderRDD.map(x => (x.->(1), 1)).foreach(x => println(x._1))
+  def orderStatusTop100(orderRDD : RDD[(Int, (String, Int, String))]) {
+    //val orderStatus = orderRDD.map(x => (x.->(1), 1)).foreach(x => println(x._1))
+    val orderStatus = orderRDD.map{x => (x._2._3, 1)}
+                              .reduceByKey{ (x, y) => x + y}
+                              .map{x => (x._2, x._1)}
+                              
+    orderStatus.sortByKey(false)
+                .foreach{x => println(x._2 + " - " + x._1)}
+  }
+  
+  def orderDate(orderRDD : RDD[(Int, (String, Int, String))]) {
+    val orderDate = orderRDD.map{x => (x._2._1, 1)}
+                            .reduceByKey{ (x, y) => x + y}
+    
+    val orderDateStatus = orderRDD.map{x => ((x._2._1, x._2._3), 1)}
+                                  .reduceByKey{ (x, y) => x + y}
+                                  .map{x => (x._1._1 , (x._1._2, x._2))}
+    
+    val orderJoin = orderDate.join(orderDateStatus)
+                              .filter{x => x._1.toString().contains("2014-07")}
+    
+    orderJoin.sortByKey(false).foreach{x => println(x._1 + " - " + x._2._2._2 + " - " + x._2._2._1)}
   }
 }
